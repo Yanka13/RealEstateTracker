711a63bfa8b70054ef8a2417ee832f4b8dfdc6e3
Daniel Sanchez <danielsangom@outlook.com>
2022-06-01 15:01:07 +0200

    rmsle and neg_rmsle are now variables

--- real_estate_tracker/pipeline.py
+++ real_estate_tracker/pipeline.py
@@ -1,4 +1,5 @@
 from sklearn.metrics import mean_squared_log_error,  make_scorer
 
-def rmsle():
-    return make_scorer(mean_squared_log_error, greater_is_better=False, squared=False)
+rmsle = make_scorer(mean_squared_log_error, greater_is_better=True, squared=False)
+
+neg_rmsle = make_scorer(mean_squared_log_error, greater_is_better=False, squared=False)
